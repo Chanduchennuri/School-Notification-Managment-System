327cded88244fd60dd27fcd455937ea32d5bc22b
fadkeabhi <31249309+fadkeabhi@users.noreply.github.com>
2023-05-14 16:13:57 +0530

    student model & basic oauth admin
    created student model and initialized oauth for admin

---
+++ backend/index.js
@@ -0,0 +1,102 @@
+require("dotenv").config();
+
+const express = require('express')
+const session = require('express-session');
+const mongoose = require('mongoose')
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const cors = require('cors');
+
+const app = express();
+
+//testing models
+const stud = require('./models/student')
+
+// Middleware to parse request bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+// Configure session
+app.use(session({
+    secret: 'your_secret_key',
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+//cors
+app.use(cors({ origin: true, credentials: true }));
+
+//oauth code
+// Configure Passport with Google OAuth credentials
+passport.use(
+    new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: '/auth/google/callback'
+    },
+        (accessToken, refreshToken, profile, done) => {
+            // Handle user authentication logic here
+            // The 'profile' parameter contains user information
+            // Call 'done' with user data to complete the authentication process
+            // console.log(profile)
+            done(null, profile);
+        })
+);
+
+
+// Serialize and deserialize user data
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    // console.log(user)
+    done(null, user);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+//Routes
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: process.env.frontend_url }),
+    (req, res) => {
+        // Redirect to the frontend after successful authentication
+        
+        
+        if(req.user._json.email==process.env.adminMail1 || req.user._json.email==process.env.adminMail2)
+        {
+            //admin login
+            req.session.role = 'admin'
+        }
+
+        console.log(req.session.role)
+
+        res.redirect(process.env.frontend_url);
+    }
+);
+
+app.get('/auth/user', (req, res) => {
+    // console.log(req.user)
+    res.json(req.user);
+});
+
+
+app.get('/auth/logout', (req, res) => {
+    req.logout((err) => { console.log(err) });
+    res.json({ logout: "true" });
+});
+
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
---
+++ backend/models/student.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+
+const studSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    parentEmail:{
+        type:String,
+        required:true
+    },
+    fName:{
+        type:String,
+        required:true
+    },
+    pName:{
+        type:String,
+        required:true
+    },
+    lName:{
+        type:String,
+        required:true
+    },
+    sPhone:{
+        type:Number
+    },
+    pPhone:{
+        type:Number
+    },
+    class:{
+        type:String
+    }
+    
+})
+
+const STUD = mongoose.model('stud', studSchema)
+module.exports = STUD
