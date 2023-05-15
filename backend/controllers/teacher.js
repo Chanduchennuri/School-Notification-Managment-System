c5c6d19315c11b1e1b4675b35dced40dcacc6a31
fadkeabhi <31249309+fadkeabhi@users.noreply.github.com>
2023-05-15 17:44:59 +0530

    added endpoints and functions for admin/teacher

--- backend/controllers/teacher.js
+++ backend/controllers/teacher.js
@@ -1,6 +1,7 @@
 const teacher = require('../models/teacher')
 
 async function createTeacher(email,fName,lName,phone){
+    let createdOrNot
     await teacher.create({
         email:email,
         fName:fName,
@@ -8,12 +9,28 @@ async function createTeacher(email,fName,lName,phone){
         phone:phone
     })
     .then(()=>{
-        return true
+        createdOrNot = true
     })
     .catch((err) => {
         console.log(err)
-        return false
+        createdOrNot = false
     })
+    return createdOrNot
+}
+
+async function updateTeacher(email,fName,lName,phone){
+    let docs
+    await teacher.updateOne({email:email},
+        {fName:fName,
+        lName:lName,
+        phone:phone})
+    .then((e) => {
+        docs = e
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+    return docs
 }
 
 async function findByEmailT(email){
@@ -32,6 +49,29 @@ async function getAllT(){
     return docs
 }
 
+async function pushClas(email,clas){
+    let isSuccess
+    await teacher.findOne({email:email})
+    .catch((err) => {
+        console.log(err)
+        isSuccess = false
+    })
+    .then((e) => {
+        if(e.class.includes(clas)){
+            isSuccess = true
+        }
+        else{
+            e.class.push(clas)
+            const result = e.save()
+            if(result){
+                isSuccess = true
+            }
+        }
+    })
+    console.log(isSuccess)
+    return isSuccess
+}
 
 
-module.exports = {createTeacher , findByEmailT, getAllT}
+
+module.exports = {pushClas , createTeacher , updateTeacher , findByEmailT, getAllT}
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,13 +1,107 @@
+//Note for testing not used admin authentication remove true from all conditions
 const express = require('express')
 const router = express.Router()
 
-router.get('/teachers',async (req,res)=> {
-    if(req.session.role==='admin'){
-        const {getAllT} = require('../controllers/teacher')
+
+router.get('/teachers', async (req, res) => {
+    if (req.session.role === 'admin') {
+        const { getAllT } = require('../controllers/teacher')
         res.json(await getAllT())
     }
-    else{
+    else {
         res.json({})
+    }
+})
+
+router.post('/teacher/create', async (req, res) => {
+    if (req.session.role === 'admin' || true) {
+        if (req.body.email &&
+            req.body.fName &&
+            req.body.lName &&
+            req.body.phone) {
+            const { createTeacher } = require('../controllers/teacher')
+            const createdOrNot = await createTeacher(req.body.email,
+                req.body.fName,
+                req.body.lName,
+                req.body.phone)
+            if (createdOrNot === true) {
+                res.json({ msg: 'Teacher Created Successfully.' })
+            }
+            else {
+                res.json({ msg: 'Teacher Created Failed.' })
+            }
+
+        }
+        else {
+            res.json({ msg: 'All Fields Are required' })
+
+        }
+    }
+    else {
+        res.json({ msg: 'Admin access only' })
+    }
+})
+
+
+
+router.post('/teacher/update', async (req, res) => {
+    if (req.session.role === 'admin' || true) {
+        if (req.body.email &&
+            req.body.fName &&
+            req.body.lName &&
+            req.body.phone) {
+            const { updateTeacher } = require('../controllers/teacher')
+            const docs = await updateTeacher(req.body.email,
+                req.body.fName,
+                req.body.lName,
+                req.body.phone)
+            if (docs) {
+                res.json({ msg: 'Teacher Updated Successfully.' })
+            }
+            else {
+                res.json({ msg: 'Teacher Updated Failed.' })
+            }
+
+        }
+        else {
+            res.json({ msg: 'All Fields Are required' })
+
+        }
+    }
+    else {
+        res.json({ msg: 'Admin access only' })
+    }
+})
+
+
+router.post('/teacher/class/add', async (req, res) => {
+    if (req.session.role === 'admin' || true) {
+        if (req.body.email &&
+            req.body.clas) {
+            const { checkClasExist } = require('../controllers/class')
+            if (await checkClasExist(req.body.clas)) {
+                const { pushClas } = require('../controllers/teacher')
+                const docs = await pushClas(req.body.email,
+                    req.body.clas)
+                if (docs) {
+                    res.json({ msg: 'Clas added Successfully.' })
+                }
+                else {
+                    res.json({ msg: 'Clas added Failed.' })
+                }
+            }
+            else {
+                res.json({ msg: 'Class not exists' })
+            }
+
+        }
+        else {
+            res.json({ msg: 'All Fields Are required' })
+
+        }
+    }
+    else {
+        res.json({ msg: 'Admin access only' })
     }
 })
 
